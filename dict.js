237abd4437b89f921538ce773e5f0172cfbd9045
Nicholas Mansfield <nickdmansfield@gmail.com>
2024-12-06 02:14:41 -0500

    Updated sample test file. Added dict file and corrected property name in LTC dataset

---
+++ dict.js
@@ -0,0 +1,14 @@
+module.exports = {
+    units: {
+        USD: 'USD',
+        SYMBOL: 'symbol'
+    },
+    orderTypes: {
+        BUY: 'buy',
+        SELL: 'sell'
+    },
+    periods: {
+        DAILY: 'DAILY',
+        WEEKLY: 'WEEKLY'
+    }
+}
--- unit-tests/analyzePurchaseLogs.js
+++ unit-tests/analyzePurchaseLogs.js
@@ -4,7 +4,7 @@
 // describe('isLimitOrderRunnable', function () {
 //   describe('Success states', function () {
 //     it('should return -1 when the value is not present', function () {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
+//       assert.equal([1, 2, 3].indexOf(4), 99);
 //     });
 //   });
   
@@ -12,7 +12,7 @@
 //     it('should return -1 when the value is not present', function () {
 //         assert.throws(() => {
 //           testRetroactively([], [{}], { initialLiquid: 10000 }); 
-//         }, Error('No initialSymbolAmount value provided in retroSettings'));
+//         }, Error('No dfdsfdfds value provided in retroSettings'));
 //     });
 //   });
 // });
